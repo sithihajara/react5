072284aa7db5987b61462a84007ab72a013fb68a
sithihajara <sithihajara833@gmail.com>
2024-07-19 09:01:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import GetData from './components/getData'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <React.Fragment>
+      <GetData />
+    </React.Fragment>
+  )
+}
+
+export default App
